8e3130195d07a7c529f072810ec155b87211e41a
aaraynee <themediashak@server14.ardourindustries.com>
2015-11-04 11:13:29 -0500

    add git ignore

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress_drone');
+define('DB_NAME', 'themedia_dji');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'themedia_dji');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'vimbly123');
